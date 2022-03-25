4fccbff1d9742e66dde5177bbd117a133630fc32
B2Ankit <ankit15ec007@gmail.com>
2022-03-25 14:27:03 +0530

    inital commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world");
+    return 1;
+}
+
+// Steps :
+// 1. Git init. (one time)
+// 2. Git status.
+// 3. git add .
+// 4. Git commit ./git ammend.
+// 5. Provide Github repo path.(one time.)
+// 6. push the change.
